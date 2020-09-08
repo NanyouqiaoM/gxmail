ea02af6232163b4c382884a1ab740b9fc43f4bb2
xiyu <1363625195@qq.com>
2020-09-08 23:26:22 +0800

    代码生成

---
+++ gxmail-product/src/main/java/com/gx/gxmail/product/controller/CategoryBrandRelationController.java
@@ -0,0 +1,89 @@
+package com.gx.gxmail.product.controller;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gx.gxmail.product.entity.CategoryBrandRelationEntity;
+import com.gx.gxmail.product.service.CategoryBrandRelationService;
+import com.gx.common.utils.PageUtils;
+import com.gx.common.utils.R;
+
+
+
+/**
+ * 品牌分类关联
+ *
+ * @author xiyu
+ * @email 1363625195@qq.com
+ * @date 2020-09-08 23:24:18
+ */
+@RestController
+@RequestMapping("product/categorybrandrelation")
+public class CategoryBrandRelationController {
+    @Autowired
+    private CategoryBrandRelationService categoryBrandRelationService;
+
+    /**
+     * 列表
+     */
+    @RequestMapping("/list")
+    //@RequiresPermissions("product:categorybrandrelation:list")
+    public R list(@RequestParam Map<String, Object> params){
+        PageUtils page = categoryBrandRelationService.queryPage(params);
+
+        return R.ok().put("page", page);
+    }
+
+
+    /**
+     * 信息
+     */
+    @RequestMapping("/info/{id}")
+    //@RequiresPermissions("product:categorybrandrelation:info")
+    public R info(@PathVariable("id") Long id){
+		CategoryBrandRelationEntity categoryBrandRelation = categoryBrandRelationService.getById(id);
+
+        return R.ok().put("categoryBrandRelation", categoryBrandRelation);
+    }
+
+    /**
+     * 保存
+     */
+    @RequestMapping("/save")
+    //@RequiresPermissions("product:categorybrandrelation:save")
+    public R save(@RequestBody CategoryBrandRelationEntity categoryBrandRelation){
+		categoryBrandRelationService.save(categoryBrandRelation);
+
+        return R.ok();
+    }
+
+    /**
+     * 修改
+     */
+    @RequestMapping("/update")
+    //@RequiresPermissions("product:categorybrandrelation:update")
+    public R update(@RequestBody CategoryBrandRelationEntity categoryBrandRelation){
+		categoryBrandRelationService.updateById(categoryBrandRelation);
+
+        return R.ok();
+    }
+
+    /**
+     * 删除
+     */
+    @RequestMapping("/delete")
+    //@RequiresPermissions("product:categorybrandrelation:delete")
+    public R delete(@RequestBody Long[] ids){
+		categoryBrandRelationService.removeByIds(Arrays.asList(ids));
+
+        return R.ok();
+    }
+
+}
