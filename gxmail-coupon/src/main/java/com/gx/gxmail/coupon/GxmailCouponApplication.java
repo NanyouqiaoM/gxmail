d3a9a8b07812e0c129f6a8a1bde1c74dc50d3c95
xiyu <1363625195@qq.com>
2020-09-09 23:35:25 +0800

    代码生成

--- gxmail-coupon/src/main/java/com/gx/gxmail/coupon/GxmailCouponApplication.java
+++ gxmail-coupon/src/main/java/com/gx/gxmail/coupon/GxmailCouponApplication.java
@@ -2,8 +2,10 @@ package com.gx.gxmail.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GxmailCouponApplication {
 
     public static void main(String[] args) {
--- gxmail-warehouse/src/main/java/com/gx/gxmail/warehouse/GxmailWarehouseApplication.java
+++ gxmail-warehouse/src/main/java/com/gx/gxmail/warehouse/GxmailWarehouseApplication.java
@@ -1,4 +1,4 @@
-package com.gx.gxmail.gxmailwarehouse;
+package com.gx.gxmail.warehouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
