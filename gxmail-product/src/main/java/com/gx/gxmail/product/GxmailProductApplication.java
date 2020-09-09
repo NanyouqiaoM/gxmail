32cc3d04dbdeb708153b9e60dba19a54fe5862a9
xiyu <1363625195@qq.com>
2020-09-09 21:12:21 +0800

    代码生成

--- gxmail-product/src/main/java/com/gx/gxmail/product/GxmailProductApplication.java
+++ gxmail-product/src/main/java/com/gx/gxmail/product/GxmailProductApplication.java
@@ -1,8 +1,9 @@
 package com.gx.gxmail.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("com.gx.gxmail.product.dao")
 @SpringBootApplication
 public class GxmailProductApplication {
 
--- gxmail-product/src/test/java/com/gx/gxmail/product/GxmailProductApplicationTests.java
+++ gxmail-product/src/test/java/com/gx/gxmail/product/GxmailProductApplicationTests.java
@@ -1,13 +1,21 @@
 package com.gx.gxmail.product;
 
+import com.gx.gxmail.product.entity.BrandEntity;
+import com.gx.gxmail.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GxmailProductApplicationTests {
+    @Autowired
+    BrandService brandService;
 
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setName("西雨");
+        System.out.println(brandService.save(brandEntity)+"====");
     }
 
 }
