ec88c54ed857dea1f0b92c33dc16d1992dc2e6ff
xiyu <1363625195@qq.com>
2020-09-12 15:48:46 +0800

    nacos注册中心

--- gxmail-coupon/src/main/java/com/gx/gxmail/coupon/GxmailCouponApplication.java
+++ gxmail-coupon/src/main/java/com/gx/gxmail/coupon/GxmailCouponApplication.java
@@ -3,6 +3,9 @@ package com.gx.gxmail.coupon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +13,8 @@ public class GxmailCouponApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GxmailCouponApplication.class, args);
+
+
     }
 
 }
